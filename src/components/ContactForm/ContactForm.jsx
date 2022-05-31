a109f5ac2b3723018dc8812e6bd5392a926c3ed6
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-31 09:20:25 +0300

    contact form onchange and submit function started

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,16 +6,29 @@ class ContactForm extends Component {
     super(props);
     this.inputNameID = `name-${nanoid()}`;
   }
+
+  addNewContact = e => {
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    const { value, name } = e.currentTarget;
+    console.log(value, name);
+  };
+
   render() {
+    const { name } = this.props;
     return (
-      <form autoComplete="off">
-        <label htmlFor=""></label>
+      <form autoComplete="off" onSubmit={this.addNewContact}>
+        <label htmlFor={this.inputNameID}></label>
         <input
           id={this.inputNameID}
           type="text"
           name="name"
+          value={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          onChange={this.handleChange}
           required
         />
         <button type="submit">Add contact</button>
