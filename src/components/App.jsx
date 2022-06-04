592b007a6a06c8e642293116fd15681e637f125a
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-04 11:09:11 +0300

    local storage load and save added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import ContactForm from 'components/ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
+import * as db from 'Service/Database';
 
 const INITIAL_STATE = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contacts: [],
   filter: '',
 };
 
 class App extends Component {
   state = { ...INITIAL_STATE };
+
+  componentDidMount() {
+    const savedContacts = db.fetchPhonebook();
+    savedContacts && this.setState({ contacts: savedContacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      db.updatePhonebook(this.state.contacts);
+    }
+  }
 
   updateContactList = newContact => {
     if (this.isContactExist(newContact)) {
