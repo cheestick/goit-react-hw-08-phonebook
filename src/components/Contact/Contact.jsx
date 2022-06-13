6f76f35467d939e066569570f0309f633644e709
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-13 16:13:02 +0300

    contact component turn to functional

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,24 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import s from './Contact.module.css';
 
-export default class Contact extends Component {
-  handleDeleteContact = e => {
-    this.props.onDeleteContact(this.props.id);
-  };
-
-  render() {
-    const { id, name, number } = this.props;
-    return (
-      <li className={s.row} id={id}>
-        <span className={s.name}>{name}:</span>
-        <span className={s.number}>{number}</span>
-        <button className={s.buttonDelete} onClick={this.handleDeleteContact}>
-          x
-        </button>
-      </li>
-    );
-  }
+export default function Contact({ id, name, number, onDeleteContact }) {
+  return (
+    <li className={s.row} id={id}>
+      <span className={s.name}>{name}:</span>
+      <span className={s.number}>{number}</span>
+      <button className={s.buttonDelete} onClick={() => onDeleteContact(id)}>
+        x
+      </button>
+    </li>
+  );
 }
 
 Contact.propTypes = {
